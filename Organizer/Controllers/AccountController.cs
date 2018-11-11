cfb091d9eae824a61d00194685a107d2e2606358
Mayster911 <krzysztofsosna@wp.pl>
2018-11-11 17:10:40 +0100

    Changed usings, cleaned solution

--- Organizer/Controllers/AccountController.cs
+++ Organizer/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Organizer.Models;
+using Organizer.ViewModels;
 
 namespace Organizer.Controllers
 {
