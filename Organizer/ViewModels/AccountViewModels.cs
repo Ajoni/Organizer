4467d50aabdbb29b6e783f55a71e3f0f574919b8
Mayster911 <krzysztofsosna@wp.pl>
2018-11-11 16:49:52 +0100

    fixed Viewmodels

--- Organizer/ViewModels/AccountViewModels.cs
+++ Organizer/ViewModels/AccountViewModels.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace Organizer.Models
+namespace Organizer.ViewModels
 {
 
     public class SendCodeViewModel
