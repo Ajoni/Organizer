32318c8b9eb23e471d8ca0d5ecb554ddfe3490fd
Mateusz <m.janel@student.pb.edu.pl>
2018-11-21 00:23:31 +0100

    Add easy Post model like in the db diagram.

---
+++ Organizer/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+public class Post
+{
+	[Key] public int postId { get; set; }
+    [ForeignKey("Group")] public int Nr { get; set; }
+    [Required] public string content { get; set; }
+
+}
