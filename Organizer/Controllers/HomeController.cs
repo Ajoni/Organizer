b9f6798f60e334f74bcb71ea50a99cfa0fe49c25
Ajoni <adam584@onet.pl>
2019-01-20 15:11:12 +0100

    home page summary

--- Organizer/Controllers/HomeController.cs
+++ Organizer/Controllers/HomeController.cs
@@ -1,20 +1,28 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Organizer.Data;
-using System;
-using System.Collections.Generic;
+using Organizer.Models;
 using System.Linq;
-using System.Net;
-using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using System;
+using Organizer.ViewModels;
 
 namespace Organizer.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
-            return View();
+            var userId = User.Identity.GetUserId();
+            var notes = db.Notes.Include(n => n.User).Where(x => x.UserId == userId).ToList();
+            var tODOItems = db.TODOItems.Include(t => t.User).Where(x => x.UserId == userId && x.EndDate> DateTime.Now).ToList();
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var events = userManager.FindById(User.Identity.GetUserId()).Events.Where(e => e.EndDate > DateTime.Now).ToList();
+            return View(new SummaryViewModel(notes,tODOItems,events));
         }
 
     }
---
+++ Organizer/ViewModels/SummaryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Organizer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Organizer.ViewModels
+{
+    public class SummaryViewModel
+    {
+        public List<Note> Notes { get; set; }
+        public List<TODOItem> TODOs { get; set; }
+        public List<UserEvent> UserEvents { get; set; }
+
+        public SummaryViewModel(List<Note> notes, List<TODOItem> tODOItems, List<UserEvent> userEvents)
+        {
+            Notes = notes;
+            TODOs = tODOItems;
+            UserEvents = userEvents;
+        }
+    }
+}
