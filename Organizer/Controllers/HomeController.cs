292b08bfd0c976e86e68dc3e612a2f91b96ec5cb
Ajoni <adam584@onet.pl>
2019-01-23 17:38:23 +0100

    bleh

--- Organizer/Controllers/HomeController.cs
+++ Organizer/Controllers/HomeController.cs
@@ -19,11 +19,11 @@ namespace Organizer.Controllers
         {
             var userId = User.Identity.GetUserId();
             var notes = db.Notes.Include(n => n.User).Where(x => x.UserId == userId).ToList();
-            var tODOItems = db.TODOItems.Include(t => t.User).Where(x => x.UserId == userId && x.EndDate> DateTime.Now).ToList();
+            var tODOItems = db.TODOItems.Include(t => t.User).ToList();
             var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
             var user = userManager.FindById(User.Identity.GetUserId());
             var events = user.Events.Where(e => e.EndDate > DateTime.Now).ToList();
-            double done = user.TodosDoneInTime == 0 ? 1 : user.TodosDoneInTime;
+            double done = user.TodosDoneInTime;
             double all = user.TodosTotal == 0 ? 1 : user.TodosTotal;
             double perf = (done/all)*100;
             return View(new SummaryViewModel(notes,tODOItems,events, Math.Round(perf,0)));
